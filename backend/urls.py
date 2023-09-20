1a88bc9da0369bd1c5021bb0d11da84baa4b4721
48845 174 <miror49735@sportrid.com>
2023-09-20 17:53:45 +0000

    START DEVELOPMENT 174TECH

--- backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url, include, static
-from django.conf import settings
-#DataFlair #DjangoCMS
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('cms.urls')),
-] + static.static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
